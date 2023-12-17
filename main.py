968c11f86c9865c9cd67c1b57bed70aadf31deff
Ralph McCracken <ralph@mccracken.work>
2023-12-17 16:13:07 -0600

    fixed typo in page title

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title='Product Correllation', page_icon='⚖️', layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title='Product Correlation', page_icon='⚖️', layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 st.caption('VACAYZEN')
 st.title('Product Correlation')
